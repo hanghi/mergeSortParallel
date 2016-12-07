8288f026d9ba2a8538268f97f3490c1c3940683b
Sherlock <14520247@gm.uit.edu.vn>
2016-12-07 22:15:11 +0700

    last update

--- MergeSortCuda/Helper.cpp
+++ MergeSortCuda/Helper.cpp
@@ -1,27 +1,27 @@
 #include "Helper.h"
 
 //Generate unsorted list
-bool generateRandomList(int length, long*& list) {
+bool generateRandomList(long length, int*& list) {
 
-	list = (long*)malloc(sizeof(long) * length);
+	list = (int*)malloc(sizeof(int) * length);
 	if (list == NULL)
 		return false;
 
 	for (int i = 0; i < length; i++)	
-		list[i] = rand() ;
+		list[i] = rand() % length ;
 	
 	return true;
 }
 
 //save result test to file
-void writeLog(string fileLog, int length, double time1, double time2, double time3) {
+void writeLog(string fileLog, int length, double time1, double time2, double time3, double time4, double time5) {
 	std::ofstream log(fileLog, std::ios_base::app | std::ios_base::out);
 	log.precision(dbl::max_digits10);
-	log << length << "\t" << fixed << time1 << "\t" << fixed << time2 << "\t" << fixed << time3 << endl;
+	log << length << "\t" << fixed << time1 << "\t" << fixed << time2 << "\t" << fixed << time3 << "\t" << fixed << time4 << "\t" << fixed << time5 << endl;
 }
 
 //print array to screen
-void showArray(long *a, int l) {
+void showArray(int *a, long l) {
 	for (int i = 0; i < l; i++) {
 		cout << a[i] << "  ";
 	}
