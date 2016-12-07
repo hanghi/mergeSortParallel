8e31d3b509d99c5b319dac2db8b81169b91848ea
Sherlock <14520247@gm.uit.edu.vn>
2016-12-07 11:22:19 +0700

    update cuda

--- MergeSortCuda/kernel.cu
+++ MergeSortCuda/kernel.cu
@@ -1,4 +1,6 @@
 //cuda libs
+
+#include <stdio.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 //local lib
@@ -7,6 +9,8 @@
 #include "MergeSortMultiThreading.h"
 //Number of threads per block
 #define THREADS_PER_BLOCK 512
+//Max thread on single block
+#define MAX_THREADS 8
 //compare 2 number, return the minimum
 __device__ long min_int(long, long);
 //Bottom up merging implementation on gpu device
@@ -22,6 +26,18 @@ double gpuMergeSort(long*, long);
 double mergeSortNormal(long*, long);
 //Merge sort multi-thread
 double mergeSortMT(long*, long);
+//Copy a part of array B to A
+__device__ void DeviceCopyArray(long* B, long* A, long l, long r);
+//Merge 2 parts of array A (left and right), result save to array B. Split by mid.
+__device__ void DeviceBottomUpMerge(long* A, long* B, int left, int mid, int right);
+//Array A[] has the items to sort; array B[] is a temporary array
+//This function can sort a part of array, start from left and end at right.
+__device__ void DeviceBottomUpMergeSort(long* A, long* B, int l, int r);
+//Thread sort on its part
+__global__ void threadMergeSortOnPart(long* a, long* temp, long n);
+//Thread merge
+__global__ void threadMerge(long* a, long* temp, long* from, long* to, long loc_size);
+double mergeSortType2(long* a, int n);
 
 //every test case, do 5 times and get average time
 void analysis(int length) {
@@ -46,7 +62,10 @@ void analysis(int length) {
 		//9 threads merge sort
 		avg_time_2 += mergeSortMT(sample, length);
 		//cuda merge sort
-		avg_time_3 += gpuMergeSort(sample, length);
+		//if (MAX_THREADS >= length)
+			//avg_time_3 += gpuMergeSort(sample, length);
+		//else
+			avg_time_3 += mergeSortType2(sample, length);
 		//
 		free(sample);
 	}
@@ -206,3 +225,194 @@ __global__ void gpu_thread_merge(long* a, long* temp, long n, long width)
 __device__ long min_int(long a, long b) {
 	return (a < b) ? a : b;
 }
+
+//------------WHEN ARRAY LENGTH IS GREATER THAN THREADS
+//Copy a part of array B to A
+__device__ void DeviceCopyArray(long* B, long* A, long l, long r)
+{
+	for (int i = l; i < r; i++)
+		A[i] = B[i];
+}
+
+//Merge 2 parts of array A (left and right), result save to array B. Split by mid.
+__device__ void DeviceBottomUpMerge(long* A, long* B, int left, int mid, int right)
+{
+	int i = left; int j = mid;
+	// While there are elements in the left or right runs...
+	for (int k = left; k < right; k++) {
+		// If left run head exists and is <= existing right run head.
+		if (i < mid && (j >= right || A[i] <= A[j])) {
+			B[k] = A[i];
+			i = i + 1;
+		}
+		else {
+			B[k] = A[j];
+			j = j + 1;
+		}
+	}
+}
+
+//Array A[] has the items to sort; array B[] is a temporary array
+//This function can sort a part of array, start from left and end at right.
+__device__ void DeviceBottomUpMergeSort(long* A, long* B, int l, int r)
+{
+	int n = r - l;
+	// Each 1-element run in A is already "sorted".
+	// Make successively longer sorted runs of length 2, 4, 8, 16... until whole array is sorted.
+	for (int width = 1; width < n; width = 2 * width)
+	{
+		// Array A is full of runs of length width.
+		for (int i = l; i < r; i = i + 2 * width)
+		{
+			// Merge two runs: A[i:i+width-1] and A[i+width:i+2*width-1] to B[]
+			// or copy A[i:n-1] to B[] ( if(i+width >= n) )
+			DeviceBottomUpMerge(A, B, i, min_int(i + width, r), min_int(i + 2 * width, r));
+		}
+		// Now work array B is full of runs of length 2*width.
+		// Copy array B to array A for next iteration.
+		// A more efficient implementation would swap the roles of A and B.
+		DeviceCopyArray(B, A, l, r);
+		//SwapArray(A, B);
+		// Now array A is full of runs of length 2*width.
+	}
+}
+//Thread sort on its part
+__global__ void threadMergeSortOnPart(long* a, long* temp, long n) {
+	//Get current ThreadID
+	int ThreadID = threadIdx.x;
+	//Get the job which is given to thread
+	long from = ThreadID *  (n / MAX_THREADS);
+	//Sort on that range
+	DeviceBottomUpMergeSort(a, temp, from,
+		from + ((ThreadID == MAX_THREADS - 1) ? (n / MAX_THREADS) + (n % MAX_THREADS) : (n / MAX_THREADS)));
+}
+//Thread merge
+__global__ void threadMerge(long* a, long* temp, long* from, long* to, long loc_size) {
+	int id = threadIdx.x * 2;
+	if (id >= loc_size)
+		return;
+	DeviceBottomUpMerge(a, temp, from[id], to[id], to[id + 1]);
+	DeviceCopyArray(temp, a, from[id], to[id + 1]);
+	long cur = from[id];
+	long cur1 = to[id + 1];
+
+	from[threadIdx.x] = cur;
+	to[threadIdx.x] = cur1;
+}
+
+double mergeSortType2(long* a, int n) {
+	//device array pointers
+	long *dev_working;
+	long *dev_temp;
+	double exetime = 0;
+	cudaError_t cudaStatus;
+
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		//goto Error;
+	}
+	//Allocation gpu memory
+	cudaStatus = cudaMalloc((void**)&dev_temp, sizeof(long) * n);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		//goto Error;
+	}
+	cudaStatus = cudaMalloc((void**)&dev_working, sizeof(long) * n);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		//goto Error;
+	}
+	//Copy local array to gpu-memory
+	cudaStatus = cudaMemcpy(dev_working, a, sizeof(long) * n, cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		//goto Error;
+	}
+	high_resolution_clock::time_point watch = high_resolution_clock::now();
+	//threads sort on its range
+	threadMergeSortOnPart << <1, MAX_THREADS >> > (dev_working, dev_temp, n);
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		//goto Error;
+	}
+
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		//goto Error;
+	}
+	duration<double> time_span = (high_resolution_clock::now() - watch);
+	exetime += time_span.count();
+	//visualization range
+	long* from = (long*)malloc(sizeof(long) * MAX_THREADS);
+	long* to = (long*)malloc(sizeof(long) * MAX_THREADS);
+
+	long n_l_jobs = n / MAX_THREADS;
+	long n_l_j_remain = n % MAX_THREADS;
+
+	Jobs l_jobs;
+	for (int i = 0; i < MAX_THREADS; i++) {
+		long current_jobs = n_l_jobs;
+		if (i == MAX_THREADS - 1) //if it's last thread, give it remain job
+			current_jobs += n_l_j_remain;
+
+		from[i] = i * n_l_jobs;
+		to[i] = i * n_l_jobs + current_jobs;
+	}
+
+
+	long *dev_from;
+	long *dev_to;
+
+	cudaStatus = cudaMalloc((void**)&dev_from, sizeof(long) * MAX_THREADS);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		//goto Error;
+	}
+	cudaStatus = cudaMalloc((void**)&dev_to, sizeof(long) * MAX_THREADS);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		//goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_from, from, sizeof(long) * MAX_THREADS, cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		//goto Error;
+	}
+	cudaStatus = cudaMemcpy(dev_to, to, sizeof(long) * MAX_THREADS, cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		//goto Error;
+	}
+	watch = high_resolution_clock::now();
+
+	for (int range = 1; range < MAX_THREADS; range *= 2) 
+		threadMerge << <1, MAX_THREADS / range >> > (dev_working, dev_temp, dev_from, dev_to, MAX_THREADS);		
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		//goto Error;
+	}
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		//goto Error;
+	}
+	time_span = (high_resolution_clock::now() - watch);
+
+	exetime += time_span.count();
+	cudaStatus = cudaMemcpy(a, dev_working, sizeof(long) * n, cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		//goto Error;
+	}
+	cudaFree(dev_working);	cudaFree(dev_temp); cudaFree(dev_from);	cudaFree(dev_to);
+	return time_span.count();
+//Error:
+//	return 0;
+}
+
